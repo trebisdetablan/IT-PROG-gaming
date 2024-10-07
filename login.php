417c44b592d895228e5dce454f18ef2eca82ca72
hailrain <76203620+trebisdetablan@users.noreply.github.com>
2024-10-07 13:20:47 +0800

    the things I worked on before

---
+++ login.php
@@ -0,0 +1,17 @@
+<html></html>
+<head>
+    <title>Log In</title>
+    <link rel="stylesheet" href="input_form.css">
+</head>
+<body>
+    <div class="form">
+        <h1 class="title_card">LOGIN</h1>
+        <form class="detail_form" action="welcome.php">
+            <input type="text" class="input" name="username" placeholder="Username"><br>
+            <input type="text" class="input" name="password" placeholder="Password"><br>
+            <br>
+            <button class="register_button" type="submit">Login</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>hellwo</title>
+</head>
+<body>
+<?php echo $_GET["username"]?> <br>
+<?php echo $_GET["password"]?>
+</body>
+</html>
