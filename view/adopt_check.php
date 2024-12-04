7fde52fe4c46960e9123e25b7c2a34f443f4a202
Yoshiii04 <dinoshino19@gmail.com>
2024-12-05 06:28:42 +0800

    commit

---
+++ view/adopt_check.php
@@ -0,0 +1,54 @@
+<?php
+// Start session
+session_start();
+
+// Include database credentials
+include '../models/dbcredentials.php';
+
+// Check if user is logged in
+if (!isset($_SESSION['username'])) {
+    header("Location: login.html");
+    exit();
+}
+
+// Get pet_id from query string
+if (!isset($_GET['pet_id'])) {
+    die("Pet ID not specified.");
+}
+$pet_id = $_GET['pet_id'];
+
+// Connect to the database
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get username from session
+$username = $_SESSION['username'];
+
+// Fetch user info
+$sql = "SELECT * FROM user_info WHERE username = ?";
+$stmt = $conn->prepare($sql);
+if ($stmt === false) {
+    die("Error preparing statement: " . $conn->error);
+}
+
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$user_result = $stmt->get_result();
+$user = $user_result->fetch_assoc();
+
+// Check if user info is complete (you can add more fields as required)
+if (empty($user['address']) || empty($user['phone_number']) || empty($user['city']) || empty($user['zip'])) {
+    // Redirect to user info update page
+    header("Location: userinfoupdate.php");
+} else {
+    // Redirect to adoption summary page
+    header("Location: userdetails.php?pet_id=$pet_id");
+}
+
+$stmt->close();
+$conn->close();
+?>
